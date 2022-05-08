5d3971790184cec42b68e601bfc28c6636177315
Manikanta7013 <shanmukamanikanta6@gmail.com>
2022-05-08 11:25:43 +0530

    deployed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Axios from 'axios';
+import React,{useState} from 'react';
 function App() {
+  const[search,setSearch]=useState("");
+  const[datas,setData]=useState([]);
+  const submitHandler=e=>{
+    e.preventDefault();
+    Axios
+    .get(`https://www.omdbapi.com/?s=${search}&apikey=db4722b6`)
+    .then((response) => {
+      console.log(response);
+      setData(response.data.Search);
+    });
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <center>
+            <h1 style={{color:"seaGreen"}}>Find Your Favorite Movies</h1>
+
+    <form  onSubmit={submitHandler}>
+      
+      <input type="text" className="container" placeholder='Search Movies' size="40" value={search} onChange={(e)=>setSearch(e.target.value)}/>&nbsp;
+      <input type="submit" className='container' value="search"/>
+     
+    </form>
+    <div className="row">
+    {datas.map(item =>
+    <div className='col-md-4'>
+        <div class="card" style={{"width": "18rem"}}>
+        <img src={item.Poster} class="card-img-top" className='max' alt={item.Title} />
+        
+         <div class="card-body">
+         <h4 className="card-title">{item.Title}</h4>
+         <h6>{item.Year}</h6>
+        </div>
+      </div>
+      </div>
+    )}
     </div>
+    </center>
+    </div>
+    
   );
 }
 
